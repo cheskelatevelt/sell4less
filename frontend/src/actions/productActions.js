b7eb913283ec07c6cb8ed8f81a2bc5c5150a3ab7
cheskelatevelt <cheskel@evelt.com>
2022-02-01 01:27:40 -0500

    after part 13

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,10 +22,16 @@ export const listProducts = () => async (dispatch) => {
 
 export const detailsProduct = (productId) => async (dispatch) => {
   dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
-  try{
-    const {data} = Axios.get(`/api/products/${productId}`);
-    dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
-  } catch(error) {
-    dispatch({type: PRODUCT_DETAILS_FAIL})
+  try {
+    const { data } = await Axios.get(`/api/products/${productId}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
